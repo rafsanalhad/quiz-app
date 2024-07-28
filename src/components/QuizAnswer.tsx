cf2a1ab165a78b8f06d452cb3d9ad347c971e799
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-07-28 15:12:05 +0700

    add cuy

--- src/components/QuizAnswer.tsx
+++ src/components/QuizAnswer.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "../Redux/store";
 interface QuizAnswerProps {
   incorrect: string;
   correct: string;
 }
 const QuizAnswer = ({ incorrect, correct }: QuizAnswerProps) => {
-  const [checkAnswer, setCheckAnswer] = React.useState(false);
   const [checkBetul, setCheckBetul] = React.useState("belum dijawab");
+  const [nilai, setNilai] = React.useState(0);
   const incorrectArray = incorrect.split(",");
+  const counter = useSelector((state: RootState) => state.counter);
+  const dispatch: AppDispatch = useDispatch();
   const answers = [];
   for (let i = 0; i < incorrectArray.length; i++) {
     answers.push(incorrectArray[i]);
@@ -22,38 +25,37 @@ const QuizAnswer = ({ incorrect, correct }: QuizAnswerProps) => {
     console.log(data);
     if (data) {
       if (data === correct) {
+        dispatch({ type: "INCREMENT_NILAI" });
         setCheckBetul("benar");
       } else {
         setCheckBetul("salah");
       }
     }
+    dispatch({ type: 'INCREMENT' });
   };
-  useEffect(() => {
-    console.log(checkAnswer);
-  }, [checkAnswer]);
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2 w-full">
       {answers.map((ans, index) => {
         let bgColor;
 
-        if (checkBetul === "belum dijawab") {
-          bgColor = "bg-[#E5E8EF]";
-        } else if (checkBetul === "benar") {
-          bgColor = "bg-green-500";
-          if (ans !== correct) {
-            bgColor = "bg-red-500";
-          }
-        } else if (checkBetul === "salah") {
-          bgColor = "bg-red-500";
-          if (ans === correct) {
-            bgColor = "bg-green-500";
-          }
-        }
+        // if (checkBetul === "belum dijawab") {
+        //   bgColor = "bg-[#E5E8EF]";
+        // } else if (checkBetul === "benar") {
+        //   bgColor = "bg-green-500";
+        //   if (ans !== correct) {
+        //     bgColor = "bg-red-500";
+        //   }
+        // } else if (checkBetul === "salah") {
+        //   bgColor = "bg-red-500";
+        //   if (ans === correct) {
+        //     bgColor = "bg-green-500";
+        //   }
+        // }
         return (
           <div
             key={index}
             onClick={handleClick}
-            className={`p-5 ${bgColor}  me-5 mb-5`}
+            className={`p-5 bg-[#E5E8EF] me-5 mb-5`}
           >
             {ans}
           </div>
